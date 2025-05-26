e44b7698b89921002f4ad15b9f0cd4879897f26e
Arito <42208098+armangido@users.noreply.github.com>
2025-05-26 17:04:29 +0400

    push all work

---
+++ script.js
@@ -0,0 +1,70 @@
+const screenshots = {
+  "index.html": "index",
+  "minecraft.html": "minecraft",
+  "fortnite.html": "fortnite",
+  "lol.html": "leagueoflegends",
+  "amongus.html": "amongus",
+  "csgo.html": "csgo",
+};
+let ss = {};
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".rating").forEach(function (el) {
+    let rating = parseFloat(el.getAttribute("data-rate"));
+    let stars = "";
+    for (let i = 1; i <= 5; i++) {
+      if (rating >= i) {
+        stars += "★";
+      } else if (rating >= i - 0.5) {
+        stars += "☆";
+      } else {
+        stars += "☆";
+      }
+    }
+    el.innerHTML = stars + " (" + rating + "/5)";
+  });
+
+  document.querySelectorAll(".nav-stars").forEach(function (el) {
+    let rating = parseFloat(el.getAttribute("data-rate")) || 0;
+    let stars = "";
+    for (let i = 1; i <= 5; i++) {
+      if (rating >= i) {
+        stars += "★";
+      } else if (rating >= i - 0.5) {
+        stars += "☆";
+      } else {
+        stars += "☆";
+      }
+    }
+    el.textContent = stars;
+  });
+});
+fetch("images/list.json")
+  .then((resp) => resp.json())
+  .then((data) => {
+    ss = data;
+    loadss();
+  });
+//Loads screenshots dynamically
+
+function loadss() {
+  const games = ["minecraft", "fortnite", "leagueoflegends", "amongus", "csgo"];
+  var name = window.location.pathname.split("/").pop();
+  var folder = screenshots[name];
+  var i = 0;
+  const main = document.querySelector("main");
+  ss[folder].forEach((elem) => {
+    const img = document.createElement("img");
+    img.src = "images/screenshots/" + folder + "/" + elem;
+    img.className = "game-img";
+    if (name === "index.html") {
+      var a = document.createElement("a");
+      a.href = games[i] + ".html";
+    } else {
+      main.appendChild(img);
+      return;
+    }
+    a.appendChild(img);
+    main.appendChild(a);
+    i++;
+  });
+}
